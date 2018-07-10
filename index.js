15a3d01ff10cd22c0353c60ff8d91c7c227e2621
Jae Yong Kim <kim900180@gmail.com>
2018-07-10 23:55:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ var recipes = {}
 var newRecipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  newRecipes = object.assign({}, object)
-  newRecipes[key] = value;
-  return newRecipes
+
 }
